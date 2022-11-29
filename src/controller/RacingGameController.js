b61f5bdafc71cbe2420368c9cbda238adc76c8f5
thewronghand <penfreak77@gmail.com>
2022-11-29 12:54:28 +0900

    refactor: 메서드 분리

--- src/controller/RacingGameController.js
+++ src/controller/RacingGameController.js
@@ -32,9 +32,7 @@ class RacingGameController {
   #handleCarNamesException(names) {
     try {
       const namesArray = names.split(SIGN.comma);
-      for (const item of namesArray) {
-        validate.userInputName(item);
-      }
+      this.#validateCarNames(namesArray);
       const racingGame = new RacingGame(namesArray);
       this.#readTrialCounts(racingGame);
     } catch (error) {
@@ -43,21 +41,31 @@ class RacingGameController {
     }
   }
 
+  #validateCarNames(names) {
+    for (const item of names) {
+      validate.userInputName(item);
+    }
+  }
+
   #handleTrialCountsException(input, game) {
     try {
       const number = Number(input);
       validate.userInputNumber(number);
-      this.#outputView.printGameResultMessage();
-      for (let i = 0; i < number; i += 1) {
-        game.move();
-        this.#outputView.printMoveResult(game); //이걸 그냥 outputView에서 하면 안되나? printGameResult(number, game)으로 game.move 반복시키고 printMoveResult
-      }
-      game.calculateWinners();
-      this.#outputView.printWinners(game);
+      this.#handleTrialCounts(number, game);
     } catch (error) {
       this.#outputView.printError(ERROR.userInputNumberInvalid);
       this.#readTrialCounts(game);
     }
+  }
+
+  #handleTrialCounts(number, game) {
+    this.#outputView.printGameResultMessage();
+    for (let i = 0; i < number; i += 1) {
+      game.move();
+      this.#outputView.printMoveResult(game); //이걸 그냥 outputView에서 하면 안되나? printGameResult(number, game)으로 game.move 반복시키고 printMoveResult
+    }
+    game.calculateWinners();
+    this.#outputView.printWinners(game);
   }
 }
 
