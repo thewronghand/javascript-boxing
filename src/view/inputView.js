120d186bb25f81354064afefe3c654ee87686b04
thewronghand <penfreak77@gmail.com>
2022-12-05 03:52:57 +0900

    feat: 사용자 입력 기능 구현

--- src/view/inputView.js
+++ src/view/inputView.js
@@ -1,3 +1,24 @@
-const inputView = {};
+const { Console } = require('@woowacourse/mission-utils');
+const { GAME_MESSAGE } = require('../utils/constants/message');
+
+const inputView = {
+  readPunch(callback) {
+    Console.readLine(GAME_MESSAGE.readUserInputPunch, (input) => {
+      callback(input);
+    });
+  },
+
+  readBlock(callback) {
+    Console.readLine(GAME_MESSAGE.readUserInputBlock, (input) => {
+      callback(input);
+    });
+  },
+
+  readCommand(callback) {
+    Console.readLine(GAME_MESSAGE.readUserInputCommand, (input) => {
+      callback(input);
+    });
+  }
+};
 
 module.exports = inputView;
