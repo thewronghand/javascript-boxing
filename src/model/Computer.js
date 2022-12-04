244934a3718bd9d57d6f094321b0b50d72d18d80
thewronghand <penfreak77@gmail.com>
2022-12-05 03:55:09 +0900

    feat: Computer 클래스 구현

--- src/model/Computer.js
+++ src/model/Computer.js
@@ -1,3 +1,30 @@
-class Computer {}
+const boxingRandomGenerator = require('../utils/boxingRandomMoveGenerator');
+const boxingRandomMoveGenerator = require('../utils/boxingRandomMoveGenerator');
+const { NUMBER } = require('../utils/constants/value');
+
+class Computer {
+  #healthPoints;
+
+  constructor() {
+    this.#healthPoints = NUMBER.initialHealthPoints;
+  }
+
+  getHealthPoints() {
+    return this.#healthPoints;
+  }
+
+  receiveDamage() {
+    this.#healthPoints = this.#healthPoints - NUMBER.hitDamage;
+  }
+
+  resetHealthPoints() {
+    this.#healthPoints = NUMBER.initialHealthPoints;
+  }
+
+  generateRandomAttackBlock() {
+    const randomMove = boxingRandomGenerator.generateRandomMove();
+    return randomMove;
+  }
+}
 
 module.exports = Computer;
